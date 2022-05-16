f79e6aec73cc28d5c978035452450b8a488fc618
Chhavi Dandriyal <99489321+chhavid@users.noreply.github.com>
2022-05-16 12:10:18 +0530

    Changed the style of glass.

--- waterSort.sh
+++ waterSort.sh
@@ -3,8 +3,7 @@ echo '{"glass1": [1,2,1],"glass2": [2,1,2],"glass3": []}' > waterSort.json
 # echo '{"glass1": [1,2,1],"glass2": [2,1,3],"glass3": [3,2,3],"glass4": []}' > waterSort.json
 # echo '{"glass1": [1,1,1],"glass2": [2,2,2],"glass3": [3,3],"glass4": [4,4,4],"glass5": [3]}' > waterSort.json
 # echo '{"glass1": [1,1,1],"glass2": [2,2,2],"glass3": [3,3],"glass4": [4,4,4],"glass5": [3],"glass6": [5,5,5]}' > waterSort.json
-  node generateHtml.js
-  open waterSort.html
+  node generateHtml.js;open waterSort.html
 
 while [[ $? == 0 ]]; do 
   read -p "Pick a glass to take water " pick
@@ -12,8 +11,7 @@ while [[ $? == 0 ]]; do
 
   node waterSort.js ${pick} ${pour}
   exitcode=$?
-  node generateHtml.js
-  open waterSort.html
+  node generateHtml.js;open waterSort.html
   echo "exit" ${exitcode} | bash
 done 
 
